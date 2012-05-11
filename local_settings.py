d6dd763272f7dadd5294b7326e0b6ef39c044a1b
Janarthanan R <rsdjjana@gmail.com>
2012-05-12 00:37:12 +0530

    addition of local and global_setting.py

---
+++ local_settings.py
@@ -0,0 +1,39 @@
+# Django settings for portal project.
+import global_settings.py
+from global_settings import*
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (('rsdjjana','rsdjjana@gmail.com'),
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'spons-portal',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'JANa1993',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+	'home/janarthananr/spons-portal/templates',
+)
